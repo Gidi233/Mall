3687ba2dae6071ee1a94dc27609e9f183de34c95
Gidi233 <1989871763@qq.com>
2024-05-31 22:07:16 +0800

    初始化zap日志库,配置日志输出规则

---
+++ utils/directory.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"Mall/global"
+	"os"
+
+	"go.uber.org/zap"
+)
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func CreateDir(dirs ...string) (err error) {
+	for _, v := range dirs {
+		exist, err := PathExists(v)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			global.GVA_LOG.Debug("create directory" + v)
+			if err := os.MkdirAll(v, os.ModePerm); err != nil {
+				global.GVA_LOG.Error("create directory"+v, zap.Any(" error:", err))
+				return err
+			}
+		}
+	}
+	return err
+}
